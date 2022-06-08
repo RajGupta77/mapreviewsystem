f6a314e1f7f9a9d057d41eb2b2f14fe7c516e030
RajGupta77 <74604730+RajGupta77@users.noreply.github.com>
2022-06-08 16:10:26 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+    userName:{
+        type:String,
+        require:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        require:true,
+    },
+    password:{
+        type:String,
+        require:true,
+        min:6,
+    }
+
+});
+module.exports=mongoose.model("user",userSchema);
